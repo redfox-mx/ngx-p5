d09ba07a959d0a6c625dc66abe11d537ac197973
redfox-mx <hernandezdj98@outlook.com>
2021-09-01 00:44:53 +0000

    build(components): allow core lib as peer dependencie

--- packages/components/src/lib/ngx-p5.component.ts
+++ packages/components/src/lib/ngx-p5.component.ts
@@ -1,12 +1,25 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Output
+} from '@angular/core';
 
 
 @Component({
   selector: 'ngx-p5',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `<div p5Element [sketch]="sketch" class="ngx-p5-component"><div>`,
+  template: `<div class="ngx-p5-component" #element><div>`,
 })
 export class NgxP5Component {
-  @Input()
-  public sketch: any;
+
+  @Output()
+  public preload: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  public setup: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  public draw: EventEmitter<any> = new EventEmitter();
 }
