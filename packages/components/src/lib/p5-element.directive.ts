b2202718ed607b566750dab3eba6d3f12e57f7ad
redfox-mx <hernandezdj98@outlook.com>
2021-09-01 00:44:26 +0000

    doc(components): add comments

--- packages/components/src/lib/p5-element.directive.ts
+++ packages/components/src/lib/p5-element.directive.ts
@@ -4,18 +4,32 @@ import { toSketch, createCanvas } from '@ngx-p5/core';
 
 import { isFunction } from './isFunction.function';
 
+/**
+ * @description
+ * Enable an easy way to attach p5 sektch to any HTMLElement
+ */
 @Directive({ selector: '[p5Element]'})
 export class P5Element implements OnDestroy, OnInit{ // eslint-disable-line @angular-eslint/directive-class-suffix
 
+  /**
+   * @description
+   * Input property. Refers to and p5 sketch to be rendered
+   */
   @Input()
   public set sketch(s: any){
     this._sketch = s;
   }
 
+  /** private sketch instance */
   private _sketch!: (p: any) => void;
 
+  /**
+   * reference to sketch and element within p5 instance, usefull to
+   * remove p5 sketches and debug parent element
+   */
   private instance: any;
 
+  /** internal DOM HTMLElement to attachp5 instance */
   private element!: HTMLElement;
 
   constructor(_ref: ElementRef<HTMLElement>){
@@ -28,6 +42,12 @@ export class P5Element implements OnDestroy, OnInit{ // eslint-disable-line @ang
     this.attach(this._sketch);
   }
 
+  /**
+   * @description
+   * method to attach custom sketch to current element
+   * @param sketch custom sketch
+   * @returns object with element and instance of p5 for current sketch
+   */
   public attach(sketch: any){
     this.removeInstance();
 
@@ -37,6 +57,10 @@ export class P5Element implements OnDestroy, OnInit{ // eslint-disable-line @ang
     return createCanvas(sketch, this.element)
   }
 
+  /**
+   * @description
+   * remove p5 instance
+   */
   public removeInstance(){
     if (!this.instance) return;
 
