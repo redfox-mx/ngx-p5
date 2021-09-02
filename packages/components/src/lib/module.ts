a6f13260c7cf4844cf153f57cb6209e3734a7afb
redfox-mx <hernandezdj98@outlook.com>
2021-09-02 00:44:31 +0000

    refactor(components): remove unused imports

--- packages/components/src/lib/module.ts
+++ packages/components/src/lib/module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgxP5Component } from './ngx-p5.component';
 import { P5Element } from "./p5-element.directive";
 @NgModule({
   imports: [CommonModule],
-  declarations: [NgxP5Component, P5Element],
-  exports: [ NgxP5Component, P5Element ]
+  declarations: [ P5Element ],
+  exports: [ P5Element ]
 })
 export class NgxP5Module {}
